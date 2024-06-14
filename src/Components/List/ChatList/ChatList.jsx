933a15ae458825f7151218fa8c00a505e27a977e
Harshith-Sai-Tumu <harshith.sai2003t@gmail.com>
2024-06-15 00:57:08 +0530

    Modified Some Styles in the interface

--- src/Components/List/ChatList/ChatList.jsx
+++ src/Components/List/ChatList/ChatList.jsx
@@ -1,24 +1,54 @@
-import React, { useState } from 'react'
-import "./chatList.css"
+import React, { useState } from "react";
+import "./chatList.css";
 const ChatList = () => {
-
-  const [addMode,setAddMode] = useState(false)
+  const [addMode, setAddMode] = useState(false);
 
   return (
-    <div className='chatlist'>
-      <div className='search'>
-        <div className='searchbar'>
+    <div className="chatlist">
+      <div className="search">
+        <div className="searchbar">
           <img src="./search.png" alt=""></img>
-          <input type="text" placeholder='Search'></input>
+          <input type="text" placeholder="Search"></input>
         </div>
-        <img className='plus' src={addMode? "./minus.png" : "./plus.png"} alt="" onClick={
-          ()=>{
-            setAddMode((prev) => !prev)
-          }
-        }></img>
+        <img
+          className="plus"
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          onClick={() => {
+            setAddMode((prev) => !prev);
+          }}
+        ></img>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt=""></img>
+        <div className="texts">
+          <span>Surya</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt=""></img>
+        <div className="texts">
+          <span>Surya</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt=""></img>
+        <div className="texts">
+          <span>Surya</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt=""></img>
+        <div className="texts">
+          <span>Surya</span>
+          <p>Hello</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatList
+export default ChatList;
