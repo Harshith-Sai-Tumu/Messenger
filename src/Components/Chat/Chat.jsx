e6e22ddbc29f226afde8dc010bdf58e39a489a01
Harshith-Sai-Tumu <harshith.sai2003t@gmail.com>
2024-06-15 01:41:19 +0530

    Added Messages Interface

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -3,13 +3,12 @@ import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-
-  const [open,setOpen] =useState(false)
-  const [text,setText] = useState("")
-  const handleEmoji = (e) =>{
-   setText(prev=>prev+e.emoji)
-   setOpen(false)
-  }
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
   return (
     <div className="chat">
       <div className="top">
@@ -27,8 +26,49 @@ const Chat = () => {
         </div>
       </div>
       <div className="center">
-
-        
+        <div className="message">
+          <img src="./avatar.png"></img>
+          <div className="texts">
+            <p>Lorem ipsum hi heloo</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          
+          <div className="texts">
+            <p>Lorem ipsum hi heloo</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png"></img>
+          <div className="texts">
+            <p>Lorem ipsum hi heloo</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          
+          <div className="texts">
+            <p>Lorem ipsum hi heloo</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png"></img>
+          <div className="texts">
+            <p>Lorem ipsum hi heloo</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          
+          <div className="texts">
+          <img src="https://images.pexels.com/photos/7408586/pexels-photo-7408586.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Hello"></img>
+            <p>Lorem ipsum hi heloo</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
       </div>
       <div className="bottom">
         <div className="icons">
@@ -36,19 +76,19 @@ const Chat = () => {
           <img src="./camera.png"></img>
           <img src="./mic.png"></img>
         </div>
-        <input type="text" value={text} placeholder="Type your message" onChange={
-          (e)=>{
-            setText(e.target.value)
-          }
-        }></input>
-        <div className='emoji'>
-          <img src="./emoji.png" onClick={
-            ()=>setOpen(prev => !prev)
-            }></img>
-            <div className="picker">
-            <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
-            </div>
-          
+        <input
+          type="text"
+          value={text}
+          placeholder="Type your message"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        ></input>
+        <div className="emoji">
+          <img src="./emoji.png" onClick={() => setOpen((prev) => !prev)}></img>
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
         </div>
         <button className="sendButton">Send</button>
       </div>
