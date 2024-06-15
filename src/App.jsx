adf529f9933dd5bc1cda0a4f378e95784848cdd5
Harshith-Sai-Tumu <harshith.sai2003t@gmail.com>
2024-06-15 16:26:59 +0530

    Added Login and Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import Chat from "./Components/Chat/Chat"
-import Detail from "./Components/Detail/Detail"
-import List from "./Components/List/List"
+import React from 'react';
+import Chat from './Components/Chat/Chat';
+import Detail from './Components/Detail/Detail';
+import List from './Components/List/List';
+import Login from './Components/login/Login';
+import Notification from './Components/notification/Notification';
 
 const App = () => {
+  const user = true;
   return (
-    <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
